0b8188e68e39bad31341f9d5bcba59ffccde38f3
Niko Suntay <suntay.3@gmail.com>
2020-10-20 15:52:22 -0400

    Added step 2 script

--- pair-exercise/BuildTables.sql
+++ pair-exercise/BuildTables.sql
@@ -100,3 +100,82 @@ INSERT INTO visit (pet_id, owner_id, visit_date, procedure) VALUES (
         '05 - EAR INFECTION');
 
 --PART 2
+
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS visit;
+DROP TABLE IF EXISTS invoice;
+DROP TABLE IF EXISTS pet_owner;
+DROP TABLE IF EXISTS pet;
+DROP TABLE IF EXISTS invoice_item;
+DROP TABLE IF EXISTS procedure;
+DROP TABLE IF EXISTS owner;
+
+CREATE TABLE pet (
+        pet_id serial NOT NULL,
+        name varchar(64) NOT NULL,
+        age int NOT NULL,
+        type varchar(16) NOT NULL,
+        CONSTRAINT pk_pet_id PRIMARY KEY (pet_id),
+        CONSTRAINT ck_pet_type CHECK (type IN ('DOG', 'CAT', 'BIRD', 'LIZARD', 'HORSE', 'RABBIT')),
+        CONSTRAINT ck_pet_age CHECK (age > 0)
+);
+CREATE TABLE owner (
+        owner_id serial NOT NULL,
+        first_name varchar(16) NOT NULL,
+        last_name varchar(16) NOT NULL,
+        address varchar(32) NOT NULL,
+        city varchar(32) NOT NULL,
+        district varchar(2) NOT NULL,
+        postal_code varchar(16) NOT NULL,
+        CONSTRAINT pk_owner_id PRIMARY KEY (owner_id)
+);
+CREATE TABLE visit (
+        visit_id serial NOT NULL,
+        pet_id integer NOT NULL,
+        owner_id integer NOT NULL,
+        procedure_id int NOT NULL,
+        visit_date date NOT NULL,
+        CONSTRAINT pk_visit_id PRIMARY KEY (visit_id)
+);
+CREATE TABLE pet_owner (
+        pet_id   integer NOT NULL,
+        owner_id integer NOT NULL,
+        CONSTRAINT pk_pet_owner PRIMARY KEY (pet_id, owner_id)
+);
+
+CREATE TABLE procedure (
+        procedure_id serial NOT NULL,
+        number int NOT NULL,
+        procedure_name varchar(32) NOT NULL,
+        CONSTRAINT pk_procedure PRIMARY KEY (procedure_id)
+);
+
+CREATE TABLE invoice (
+        invoice_number serial NOT NULL,
+        invoice_date date NOT NULL,
+        total money NOT NULL,
+        tax_rate int DEFAULT 8 NOT NULL,
+        total_charge money NOT NULL,
+        owner_id int,
+        CONSTRAINT pk_invoice PRIMARY KEY (invoice_number)         
+);
+
+CREATE TABLE invoice_item (
+        invoice_item_id serial NOT NULL,
+        pet_id int NOT NULL,
+        procedure_id int NOT NULL,
+        amount money NOT NULL,
+        CONSTRAINT pk_invoice_item PRIMARY KEY (invoice_item_id)
+);                
+
+ALTER TABLE pet_owner ADD FOREIGN KEY (pet_id)   REFERENCES pet(pet_id);
+ALTER TABLE pet_owner ADD FOREIGN KEY (owner_id) REFERENCES owner(owner_id);
+ALTER TABLE visit ADD FOREIGN KEY (pet_id) REFERENCES pet(pet_id);
+ALTER TABLE visit ADD FOREIGN KEY (owner_id) REFERENCES owner(owner_id);
+ALTER TABLE visit ADD FOREIGN KEY (procedure_id) REFERENCES procedure(procedure_id);
+ALTER TABLE invoice ADD FOREIGN KEY (owner_id) REFERENCES owner(owner_id);
+ALTER TABLE invoice_item ADD FOREIGN KEY (procedure_id) REFERENCES procedure(procedure_id);
+ALTER TABLE invoice_item ADD FOREIGN KEY (pet_id) REFERENCES pet(pet_id);
+
+COMMIT;
